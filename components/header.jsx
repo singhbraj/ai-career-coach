676812f7dffcd4b77a72a684c2c3d605dc74f6de
singhbraj <braj333singh@gmail.com>
2025-02-15 11:12:40 +0530

    add header

---
+++ components/header.jsx
@@ -0,0 +1,33 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Sensai Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+    // <div>
+
+    // </div>
+  );
+}
